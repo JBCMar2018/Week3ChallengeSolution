2ed5e63dc1be9854e290b9ee49b694ca29ca0528
aoa4eva <hi@aoa4eva.me>
2018-04-13 14:11:42 -0400

    Updated to include return book and working menu

--- src/me/afua/Main.java
+++ src/me/afua/Main.java
@@ -3,44 +3,32 @@ package me.afua;
 import java.util.Scanner;
 
 public class Main {
+    //Creates a class variable which will always be available, so that you can keep track of the ID. You won't
+    //need this for Spring, though!
+    private static int ID=1;
 
     public static void main(String[] args) {
         //This application uses methods instead of having everything in main
-        int ID=1;
-
         Scanner keyboard = new Scanner(System.in);
         Library thisLibrary = new Library();
         boolean stop=false;
-        String yesno;
-
+        String exit;
 
         //Show the menu
-        System.out.println(showMenu());
+        do{
 
+        if(thisLibrary.getBooks().size()<1)
+        {
+            System.out.println("Read through the code in this project and complete the solution! Have fun. ☺");
+            System.out.println("There are no books in this library. Please add one");
+            addBook(thisLibrary);
+        }
 
-        //Add a book
-        addBook(thisLibrary,ID);
-
-
-        //List books
-//        System.out.println(listBooks(thisLibrary));
-//
-//        //Borrow a book
-//        System.out.println("Enter a book ID");
-//        long id = keyboard.nextLong();
-//        keyboard.nextLine();
-//        Book found = new Book();
-//
-//        for (Book toFind:thisLibrary.getBooks()) {
-//            if(toFind.getId()==id)
-//            {
-//                found = toFind;
-//            }
-//
-//        }
-//        if(found.isBorrowed())
-//        found.setBorrowed(true);
-
+        else{
+            System.out.println(showMenu());
+            getInput(keyboard,thisLibrary);
+        }
+        }while(!stop);
 
     }
 
@@ -51,6 +39,35 @@ public class Main {
            builder.append("2. Add a book\n");
            builder.append("3. Borrow a book\n");
            return builder.toString();
+    }
+    public static void getInput(Scanner keyboard, Library thisLibrary)
+    {
+        switch(keyboard.nextInt())
+        {
+            case 1:
+                System.out.println("List Books");
+                System.out.println(listBooks(thisLibrary));
+                break;
+
+            case 2:
+                System.out.println("Add a Book");
+                addBook(thisLibrary);
+                break;
+
+            case 3:
+                System.out.println("Borrow a Book");
+                borrowBook(keyboard,thisLibrary);
+                break;
+
+            case 4:
+                System.out.println("Return a Book");
+                //Complete this!! Yes. I mean you. Reading. Hint: Look through the code.
+                break;
+            case 5:
+                System.exit(0);
+                break;
+
+        }
     }
 
     public static Book newBook(int id) {
@@ -84,14 +101,41 @@ public class Main {
             builder.append("Title:"+eachBook.getTitle()+"\n");
             builder.append("Author:"+eachBook.getAuthor()+"\n");
             builder.append("Year of Publication:"+eachBook.getYearPub()+"\n");
-            builder.append("Status:"+(eachBook.isBorrowed()?"Borrowed":"Available"));
+            builder.append("Status: "+(eachBook.isBorrowed()?"Borrowed":"Available")+"\n");
             builder.append("ISBN:"+eachBook.getIsbn()+"\n\n");
 
         }
         return builder.toString();
     }
 
-    public static void addBook(Library thisLibrary, int ID)
+    //You can overload methods AS WELL as constructors. This means you can have multiple methods of the same name, and pass
+    //DIFFERENT variables to them. You can't have two methods with the same method signature, though. So you can't have two
+    //methods that look like method(String, String) - they should ALWAYS accept different data types.
+    public static String listBooks(boolean available, Library library)
+    {
+        //List books by availability.
+
+        StringBuilder builder = new StringBuilder();
+
+        for(Book eachBook:library.getBooks())
+        {
+            //If the book is borrowed, it is not available.
+            //by extension, if it is not borrowed, then it is available.
+            //Read that again. :0)
+            if(eachBook.isBorrowed()==!available)
+            {
+                builder.append("Book ID:"+eachBook.getId()+"\n");
+                builder.append("Title:"+eachBook.getTitle()+"\n");
+                builder.append("Author:"+eachBook.getAuthor()+"\n");
+                builder.append("Year of Publication:"+eachBook.getYearPub()+"\n");
+                builder.append("Status: "+(eachBook.isBorrowed()?"Borrowed":"Available")+"\n");
+                builder.append("ISBN:"+eachBook.getIsbn()+"\n\n");
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void addBook(Library thisLibrary)
     {
         Scanner keyboard = new Scanner(System.in);
         boolean stop = false;
@@ -101,14 +145,69 @@ public class Main {
             thisLibrary.addBook(newBook(ID));
             System.out.println("Would you like to enter another book?");
             yesno = keyboard.nextLine();
-            if(yesno.equalsIgnoreCase("n")||yesno.equalsIgnoreCase("nO"))
+            if(yesno.equalsIgnoreCase("n")||yesno.equalsIgnoreCase("no"))
             {
                 stop = true;
+            }
+            else{
+                stop = false;
             }
             ID++;
         }while(!stop);
     }
 
+    public static void borrowBook(Scanner keyboard, Library thisLibrary)
+    {
 
+        System.out.println("This is the list of available books: ");
+        //Show available books
+        System.out.println(listBooks(true,thisLibrary));
+        System.out.println("Enter a book ID");
+        long id = keyboard.nextLong();
+        keyboard.nextLine();
+        Book found = new Book();
+
+        for (Book toFind:thisLibrary.getBooks()) {
+            if(toFind.getId()==id)
+            {
+                found = toFind;
+                System.out.println("ID: "+toFind.getId()+"Title:"+ toFind.getTitle() +" has been found\n");
+            }
+
+        }
+        if(!found.isBorrowed())
+            found.setBorrowed(true);
+        else
+        {
+            System.out.println("This book has already been borrowed");
+        }
+        thisLibrary.getBooks().set(thisLibrary.getBooks().indexOf(found),found);
+        System.out.println(listBooks(thisLibrary));
+    }
+
+    public static void returnBook(Scanner keyboard, Library thisLibrary)
+    {
+
+        System.out.println("Enter a book ID");
+        long id = keyboard.nextLong();
+        keyboard.nextLine();
+        Book found = new Book();
+
+        for (Book toFind:thisLibrary.getBooks()) {
+            if(toFind.getId()==id)
+            {
+                found = toFind;
+                System.out.println("ID: "+toFind.getId()+"Title:"+ toFind.getTitle() +" has been found\n");
+            }
+
+        }
+        if(found.isBorrowed())
+            found.setBorrowed(false);
+        else{
+            System.out.println("This book is already available");
+        }
+        thisLibrary.getBooks().set(thisLibrary.getBooks().indexOf(found),found);
+        System.out.println(listBooks(thisLibrary));
+    }
 
 }
